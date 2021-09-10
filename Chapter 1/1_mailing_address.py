c4fdc3023b292f75c07faf80e8829dd468d9a219
aisha-rm <aisharm13@gmail.com>
2021-09-10 19:12:59 +0100

    updated commit

---
+++ Chapter 1/1_mailing_address.py
@@ -0,0 +1,8 @@
+# 1 name and address
+"""Create a program that displays your name and complete mailing address. The address
+should be printed in the format that is normally used in the area where you live. Your
+program does not need to read any input from the user."""
+
+print("Aisha Rabiu Mohammed")
+print("Flat 19,\nGarrick House,\n76 High Street,\nPeterborough\nPE2 8DR")
+
