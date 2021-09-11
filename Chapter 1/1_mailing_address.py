06a2844bfcc7c5258745046a7f821c6c888d22a0
aisha-rm <aisharm13@gmail.com>
2021-09-11 18:01:53 +0100

    updated commit

--- Chapter 1/1_mailing_address.py
+++ Chapter 1/1_mailing_address.py
@@ -3,6 +3,8 @@
 should be printed in the format that is normally used in the area where you live. Your
 program does not need to read any input from the user."""
 
-print("Aisha Rabiu Mohammed")
-print("Flat 19,\nGarrick House,\n76 High Street,\nPeterborough\nPE2 8DR")
+name = "Aisha Mohammed"
+address = "No 5 Guava Close,\nOff Jabi Road,\nKaduna."
+print(name)
+print(address)
 
