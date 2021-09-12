fef6fde71f11cb77920c9dee2e59256aa76fe7d3
aisha-rm <aisharm13@gmail.com>
2021-09-12 18:59:35 +0100

    initial commit

---
+++ Chapter 1/7_Sum of the First n Positive Integers.py
@@ -0,0 +1,10 @@
+"""
+Write a program that reads a positive integer, n, from the user and then displays the
+sum of all of the integers from 1 to n. The sum of the first n positive integers can be
+computed using the formula:
+sum = (n)(n + 1)/2
+"""
+
+n = int(input("Enter any number: "))
+sumInt = (n)*(n + 1)/2
+print("The sum of all positive numbers from 1 to that number is", sumInt)
