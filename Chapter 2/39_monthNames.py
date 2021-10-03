9b9dcd3123347f0f7b2f5033e33c674c4b549e2f
aisha-rm <aisharm13@gmail.com>
2021-10-03 22:55:52 +0100

    updated commit

--- Chapter 2/39_monthNames.py
+++ Chapter 2/39_monthNames.py
@@ -4,3 +4,14 @@ a program that reads the name of a month from the user as a string. Then your
 program should display the number of days in that month. Display “28 or 29 days”
 for February so that leap years are addressed.
 """
+
+month = input("Enter the name of a month: ")
+month = month.title()
+
+thirty = ['September', "April", 'June', 'November']
+if month == 'February':
+    print("This month has 28 or 29 days.")
+elif month in thirty:
+    print("This month has 30 days.")
+else:
+    print("This month has 31 days.")  
